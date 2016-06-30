d77b1fd0694bb282361543930eb86f32aee146f7
Marco Dsoft <Marco Dsoft>
2016-06-30 14:57:18 +0200

    Added billboard.cs for billboarding purposes

---
+++ Assets/Shaders/Billboard/billboard.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class billboard : MonoBehaviour
+{
+
+	void Start ()
+    {
+        if (billboard.cam == null)
+        {
+            cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+        }
+    }
+
+    public static Transform cam;
+    public Vector3 freeRotation = Vector3.one;
+    Vector3 eangles = Vector3.zero;
+
+    void LateUpdate()
+    {
+        this.transform.LookAt(billboard.cam);
+        transform.Rotate(0, 180, 0);
+        eangles = transform.eulerAngles;
+        eangles.x *= freeRotation.x;
+        eangles.y *= freeRotation.y;
+        eangles.z *= freeRotation.z;
+        transform.eulerAngles = eangles;
+    }
+}
