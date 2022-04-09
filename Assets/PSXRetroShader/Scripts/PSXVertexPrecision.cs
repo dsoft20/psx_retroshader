ea8340f3d517305534d56ffbfd2bea762b89cd43
Mufalder <sadasyk97@gmail.com>
2022-04-10 00:49:07 +0400

    Updated PSXVertexPrecision.cs script

--- Assets/PSXRetroShader/Scripts/PSXVertexPrecision.cs
+++ Assets/PSXRetroShader/Scripts/PSXVertexPrecision.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 
 /// <summary>
-/// Sets vertex snapping positions.
+/// Sets vertex snapping precision.
 /// </summary>
 [ExecuteInEditMode]
 public class PSXVertexPrecision : MonoBehaviour
@@ -12,16 +12,44 @@ public class PSXVertexPrecision : MonoBehaviour
 
     [SerializeField, Range(0, 1024)]
     private int xPrecision = 480;
+    public int XPrecision
+    {
+        get => xPrecision;
+        set
+        {
+            if (xPrecision == value)
+                return;
+
+            xPrecision = value;
+            UpdatePrecision();
+        }
+    }
     [SerializeField, Range(0, 1024)]
     private int yPrecision = 480;
+    public int YPrecision
+    {
+        get => yPrecision;
+        set
+        {
+            if (yPrecision == value)
+                return;
+
+            yPrecision = value;
+            UpdatePrecision();
+        }
+    }
 
     private void Start()
     {
-        Shader.SetGlobalFloat("_XPrecision", xPrecision);
-        Shader.SetGlobalFloat("_YPrecision", yPrecision);
+        UpdatePrecision();
     }
 
     private void OnValidate()
+    {
+        UpdatePrecision();
+    }
+
+    private void UpdatePrecision()
     {
         Shader.SetGlobalFloat("_XPrecision", xPrecision);
         Shader.SetGlobalFloat("_YPrecision", yPrecision);
